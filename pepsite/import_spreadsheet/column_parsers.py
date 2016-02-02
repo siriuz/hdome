243e9f22ea7b4abe1e19e93100077bd15dbf79f0
RJ <surrealz@gmail.com>
2016-02-03 08:54:43 +1100

    Added pandas read csv code

--- pepsite/import_spreadsheet/column_parsers.py
+++ pepsite/import_spreadsheet/column_parsers.py
@@ -1,19 +1,4 @@
-""" Parsers that are applied (using pandas) on a column-wise basis when importing Protein Pilot files """
-
-#  Mapping of header column names to internal data field identifiers
-v5header_datafield_mappings = {
-            "Accessions": "protein_uniprot_ids",
-                 "Names": "protein_descriptions",
-                  "Conf": "idestimate_confidence",
-              "Sequence": "peptide_sequence",
-         "Modifications": "ptms_description",
-                 "dMass": "ion_delta_mass",
-                "Obs MW": "idestimate_precursor_mass",
-               "Obs m/z": "ion_mz",
-               "Theor z": "ion_charge",
-              "Spectrum": "ion_spectrum",
-              "Acq Time": "ion_retention_time"
-}
+""" Parsers that are applied (using pandas) on a column-wise basis when importing ProteinPilot files """
 
 
 def accessions_to_uniprot_list(input_string):
@@ -27,9 +12,11 @@ def accessions_to_uniprot_list(input_string):
     output_list = []
     accession_list = input_string.split(';')
     for accession in accession_list:
-        #  Checks that there are actually uniprot IDs to add
-        if (len(accession.split('|')) > 1) and (len(accession.split('|')[1])):
-            output_list.append(accession.split('|')[1]) # Return the uniprot ID which lies between the | |
+        tokens = accession.split('|')
+
+        #  Check that there are actually uniprot IDs to add
+        if (len(tokens) > 1) and (len(tokens[1]) > 0):
+            output_list.append(tokens[1])  # Return the uniprot ID which lies between the | |
 
     return output_list
 
@@ -43,7 +30,7 @@ def names_to_protein_descriptions(input_string):
     output_list = []
     protein_list = input_string.split(';')
     for protein in protein_list:
-        if (len(protein)):
+        if len(protein) > 0:
             output_list.append(protein)
 
     return output_list
